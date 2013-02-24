4517fef426b8a5a2eec433608f76618f952fc439
Borge Strand-Bergesen <borge.strand@gmail.com>
2013-02-24 12:52:51 +0100

    Responding to MS automatic syntax checker...

--- test01/hello.c
+++ test01/hello.c
@@ -20,13 +20,14 @@
  */
 
 #include <ntddk.h>
+
+DRIVER_UNLOAD DriverUnload;
  
 void DriverUnload(PDRIVER_OBJECT pDriverObject) {
 	DbgPrint("hello.sys: Goodbye world\n");
 }
  
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
-	DRIVER_UNLOAD DriverUnload;
 	DriverObject->DriverUnload = DriverUnload;
 
 	DbgPrint("hello.sys: Hello world\n");
